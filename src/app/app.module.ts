9b5e1e39c42cda4ff18e0812dc7a0695c174bc60
angel mendoza <angel@virtualsystem.com.ve>
2017-05-24 01:29:52 -0400

    se agrego un navbar simple de bootstrap con la ruta de nuevo cliente funcional, se creo un card para mejorar los estilos, los colores no son fijos, pretendo cambiarlos pero despues de ponerlos a botacion, centrarce en la estetica y en las pequeñas tranciciones que ocurren con el hover en el card y en el botn de guardar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,23 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { LocalStorageModule } from 'angular-2-local-storage';
 import { routing, appRoutingProviders } from './app.routing';
+import { RouterModule, Routes } from '@angular/router';
+
+
 
 import { AppComponent } from './app.component';
 import { MyCustomerComponent } from './my-customer-component/my-customer-component.component';
 import { MyCustomerAddComponent } from './my-customer-add/my-customer-add.component';
 import { MyCustomerEditComponent } from './my-customer-edit/my-customer-edit.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MyCustomerComponent,
     MyCustomerAddComponent,
-    MyCustomerEditComponent
+    MyCustomerEditComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
